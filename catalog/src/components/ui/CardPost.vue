2786d99e4b44be36dc76d06bc1fb50403983856f
TekinaLiadon <64956660+TekinaLiadon@users.noreply.github.com>
2021-10-02 15:32:56 +0300

    card +axios

---
+++ catalog/src/components/ui/CardPost.vue
@@ -0,0 +1,30 @@
+<template>
+    <el-card class="box-card">
+        <template #header>
+            <div class="card-header">
+                <span>{{title}}</span>
+                <div>{{id}}</div>
+            </div>
+        </template>
+        <div class="text item">
+            {{body}}
+        </div>
+    </el-card>
+</template>
+
+<script>
+    export default {
+        name: "CardPost",
+        props: {
+            title: String,
+            id: String,
+            body: String,
+        }
+    }
+</script>
+
+<style scoped>
+.box-card {
+	margin: 5px;
+}
+</style>
--- catalog/src/views/Home.vue
+++ catalog/src/views/Home.vue
@@ -1,15 +1,55 @@
 <template>
-  <div class="home">
-   Пока тестовое
-  </div>
+    <el-row class="row-bg" justify="center">
+        <el-col :span="20">
+            <el-row class="row-bg" justify="center">
+                <template v-for="(card, index) in cards" :key="index">
+                    <el-col :span="5">
+                        <CardPost :id="card.id" :title="card.title" :body="card.body"/>
+                    </el-col>
+                </template>
+            </el-row>
+        </el-col>
+    </el-row>
 </template>
 
 <script>
 
-export default {
-  name: 'Home',
+    import CardPost from "../components/ui/CardPost";
+    import axios from "axios"
 
-}
+    export default {
+        name: 'Home',
+        components: {
+            CardPost
+        },
+        data () {
+            return {
+                cards: [],
+                page: 1,
+                limit: 10,
+                totalPage: 0,
+            }
+        },
+        methods: {
+            async fetchPosts() {
+                try {
+                    const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+                        params: {
+                            _page: this.page,
+                            _limit: this.limit,
+                        }
+                    })
+                    this.totalPage = Math.ceil(response.headers['x-total-count'] / this.limit)
+                    this.cards = response.data
+                } catch (e) {
+                    console.log('Error')
+                }
+            }
+        },
+        mounted() {
+            this.fetchPosts();
+        },
+    }
 </script>
 
 <style scoped>
